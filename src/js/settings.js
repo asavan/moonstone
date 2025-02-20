c508772c526d923b90f7df320ffb7bd7ae755f12
a.savanovich <asavanster@gmail.com>
2025-02-21 02:28:08 +0300

    remove unused

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,10 +1,5 @@
 export default {
-    modes: ["ai"],
-    mode: "ai",
-    wsPort: 8088,
-    loggerAnchor: ".log",
     dataName: "myData",
-    logLevel: 4,
     daylimit: 3,
     seed: "",
 };
--- src/js/utils/shuffle.js
+++
@@ -1,8 +0,0 @@
-export function shuffleArray(array, rngFunc) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(rngFunc() * (i + 1));
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
